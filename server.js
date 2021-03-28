fa8262e182df4771ad88a28fa9b13d33760485c4
DevMasteryLabs <develop.devmastery@gmail.com>
2021-03-28 18:09:10 +0100

    Set up Express

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    res.end('Hello from a Basic Node HTTP Server');
+const app = express();
+
+app.use((req, res) => {
+    res.json({ message: "Hello from Express" });
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
-
-server.listen(process.env.PORT || 5000);
