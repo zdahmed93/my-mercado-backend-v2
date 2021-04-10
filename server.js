94ce1f0649668814079d4272638feda1954100d1
DevMasteryLabs <develop.devmastery@gmail.com>
2021-04-10 20:59:28 +0100

    Update and delete items

--- server.js
+++ server.js
@@ -1,41 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const { itemValidator } = require('./utilities/validators');
 const Item = require('./models/Item');
 
 require('dotenv').config();
 
-const { itemValidator } = require('./utilities/validators');
-
-let itemsForSale = [
-    {
-        _id: '5fcc1422e17929040331de0e',
-        title: 'Camera Pro',
-        description: 'Canon 5D Mark IV',
-        photo: 'https://devmastery-assets.vercel.app/marketplace/camera-pro.png',
-        price: 290,
-        user: '5e74cd8c7e03d41011b9331a' // This is a user id
-    },
-    {
-      _id: '5e748a4b4c903807294d5aae',
-      title: 'T-shirt',
-      description: 'A dark blue T-shirt. Its size is Large (L)',
-      photo: 'https://devmastery-assets.vercel.app/marketplace/t-shirt.jpeg',
-      price: 20,
-      user: '5e74cdf77e03d41011b9331b'
-    },
-    {
-        _id: '507f1f77bcf86cd799439011',
-        title: 'iPhone',
-        description: 'An iPhone 7 Plus Silver',
-        photo: 'https://devmastery-assets.vercel.app/marketplace/iphone7.jpeg',
-        price: 350,
-        user: '5e74cd8c7e03d41011b9331a'
-    }
-];
-
 const app = express();
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -84,46 +55,42 @@ app.post('/items', async (req, res) => {
     }
 })
 
-app.put('/items/:id', (req, res) => {
-    const itemToUpdateId = req.params.id
-    const validationResult = itemValidator.validate(req.body, { abortEarly: false })
-    if (validationResult.error) {
-        res.json(validationResult)
-    } else {
-        let itemFound = false
-        itemsForSale = itemsForSale.map(item => {
-            if (item._id === itemToUpdateId) {
-                itemFound = true
-                return {
-                    _id: item._id,
-                    ...req.body
-                }
-            } else {
-                return item
-            }
-        })
-        if (itemFound) {
-            res.json({message: "Item updated successfully"})
+app.put('/items/:id', async (req, res) => {
+    try {
+        const itemToUpdateId = req.params.id
+        const validationResult = itemValidator.validate(req.body, { abortEarly: false })
+        if (validationResult.error) {
+            res.json(validationResult)
         } else {
-            res.status(404).json({error: "Item not found"})
-        }
+            const item = await Item.findByIdAndUpdate(itemToUpdateId, req.body)
+            if (!item) {
+                res.status(404).json({error: "Item not found"})
+            } else {
+                res.status(200).json({message: "Item updated successfully"})
+            }
+        }     
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
-app.delete('/items/:id', (req, res) => {
-    const itemToDeleteId = req.params.id
-    const itemToDelete = itemsForSale.find(item => item._id === itemToDeleteId)
-    if (!itemToDelete) {
-        res.status(404).json({error: "Item not found"})
-    } else {
-        itemsForSale = itemsForSale.filter(item => item._id !== itemToDeleteId)
-        res.json({message: "Item deleted successfully"})
+app.delete('/items/:id', async (req, res) => {
+    try {
+        const itemToDeleteId = req.params.id
+        const result = await Item.deleteOne({ _id: itemToDeleteId })
+        if (result.deletedCount === 1) {
+            res.json({message: "Item deleted successfully"})
+        } else {
+            res.status(404).json({error: "Item not found"})
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
         .then(() => {
             console.log('Successfully connected to MongoDB');
             app.listen(PORT, () => {
