3e3145e54b44faa2ef212c9bce2a7ec846283209
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-25 23:11:05 +0100

    Make joi validator accept any domain for email validation (TLD stands for Top Level Domain). By default, the TLD must be a valid name listed on the IANA registry.

--- utilities/validators.js
+++ utilities/validators.js
@@ -10,12 +10,12 @@ const itemValidator = Joi.object({
 const registerValidator = Joi.object({
     firstName: Joi.string().required(),
     lastName: Joi.string(),
-    email: Joi.string().email().required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
     password: Joi.string().required().min(4)
 })
 
 const loginValidator = Joi.object({
-    email: Joi.string().email().required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
     password: Joi.string().required()
 })
 
